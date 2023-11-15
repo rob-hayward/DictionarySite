bd50e2bc9ea032cf9860eb19ba304675849c88a5
Rob <hayward.m.rob@gmail.com>
2023-11-15 11:37:10 +0000

    backend project essentially finished, ready for css styling and switching to postgresql database.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+// Import dependencies and components
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AuthContext from './AuthContext';
+import ProtectedRoute from './ProtectedRoute';
+import { configureAxios } from './axiosConfig';
+
+
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import Dashboard from './Dashboard';
+import CreateDefinable from './CreateDefinable';
+import DefinableDictionary from './DefinableDictionary';
+import DefinableDetail from './DefinableDetail';
+
+// Configure axios instance
+configureAxios();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Manage authentication state using local storage
+    const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('access_token'));
+
+    const login = () => {
+        setIsAuthenticated(true);
+    };
+
+    const logout = () => {
+        setIsAuthenticated(false);
+        localStorage.removeItem('access_token');
+    };
+
+    // Render application with authentication context and routing
+    return (
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+            <Router>
+                <Navbar isLoggedIn={isAuthenticated} />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<ProtectedRoute />}>
+                        {/* Assuming ProtectedRoute renders its children, the Dashboard will be nested inside it */}
+                        <Route index element={<Dashboard />} />
+                        <Route path="create_definable" element={<CreateDefinable />} />
+                    </Route>
+                    <Route path="/dashboard/dictionary" element={<ProtectedRoute />}>
+                        <Route index element={<DefinableDictionary />} />
+                    </Route>
+                    <Route path="/dashboard/definable_detail/:id" element={<ProtectedRoute />}>
+                        <Route index element={<DefinableDetail />} />
+                    </Route>
+                </Routes>
+            </Router>
+        </AuthContext.Provider>
+    );
 }
 
 export default App;
